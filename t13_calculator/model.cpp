f3bbae31d0256fb1ad5b4933cf6c1672efe9b62d
Brandon <bmhgxb@mst.edu>
2018-02-14 10:43:53 -0600

    Add getter and setter functions.

--- t13_calculator/model.cpp
+++ t13_calculator/model.cpp
@@ -4,3 +4,14 @@ model::model()
 {
 
 }
+
+string model::get_equation()
+{
+    return m_equation;
+}
+
+void model::add_digit(const double new_digit)
+{
+    m_result *= 10;
+    m_result += new_digit;
+}
--- t13_calculator/model.h
+++ t13_calculator/model.h
@@ -1,11 +1,20 @@
 #ifndef MODEL_H
 #define MODEL_H
-
+#include <string>
+using namespace std;
 
 class model
 {
 public:
     model();
+    string get_equation();
+    void add_digit(const double new_digit);
+
+private:
+    string m_equation;
+    update_equation();
+    double m_result;
+
 };
 
 #endif // MODEL_H
