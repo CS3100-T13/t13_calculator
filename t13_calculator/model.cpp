e44a6bcc5847ce979fbe1467e6a93fece540ab74
Brandon <bmhgxb@mst.edu>
2018-02-20 18:28:26 -0600

    Added public interface functions.

--- t13_calculator/model.cpp
+++ t13_calculator/model.cpp
@@ -2,7 +2,17 @@
 
 model::model()
 {
+  nc = new NotificationCenter();
+}
 
+
+void model::update_equation() {
+
+}
+
+model::~model()
+{
+    delete nc;
 }
 
 string model::get_equation()
@@ -14,4 +24,25 @@ void model::add_digit(const double new_digit)
 {
     m_result *= 10;
     m_result += new_digit;
+
+    update_equation();
+
+    nc->post( EQUATION_UPDATED );
+}
+
+
+void model::mathFunctionEntered( const MathFunction f ) {
+    funcs.push_back( f );
+
+    update_equation();
+
+    nc->post( EQUATION_UPDATED );
+}
+
+void model::specialKeyEntered( const SpecialKey s ){
+    specials.push_back( s );
+
+    update_equation();
+
+    nc->post( EQUATION_UPDATED );
 }
--- t13_calculator/model.h
+++ t13_calculator/model.h
@@ -1,19 +1,47 @@
 #ifndef MODEL_H
 #define MODEL_H
 #include <string>
+#include <vector>
+#include "notificationcenter.h"
 using namespace std;
+
+const NotificationID EQUATION_UPDATED = "NOTIFICATION_UPDATED";
+
+enum MathFunction {
+    UNDEFINED = 0,
+    PLUS = 1,
+    MINUS = 2,
+    MULTIPLY = 3,
+    DIVIDE = 4
+};
+
+enum SpecialKey {
+    //use isdigit(int c), built in function for c++ somehow//
+    EQUALS = 0,
+    ALL_CLEAR = 1,
+    OPEN_PAREN = 2,
+    CLOSE_PAREN = 3,
+    DECIMAL = 4,
+    SIGN_SWITCH = 5
+};
 
 class model
 {
 public:
     model();
+    ~model();
     string get_equation();
     void add_digit(const double new_digit);
+    void mathFunctionEntered( const MathFunction f );
+    void specialKeyEntered( const SpecialKey s );
 
 private:
     string m_equation;
-    update_equation();
+    void update_equation();
     double m_result;
+    NotificationCenter *nc;
+    vector<MathFunction> funcs;
+    vector<SpecialKey> specials;
 
 };
 
